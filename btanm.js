94bb7359d2d071a34c4ce657e2e3f801db3f8bbe
PSalleSDev <74500251+PSalleSDev@users.noreply.github.com>
2022-11-15 23:54:56 -0300

    Update btanm.js

--- btanm.js
+++ btanm.js
@@ -1,23 +1,21 @@
-module.exports = (url, modules, callback) => {
-    modules.request(false).get(url + "/download", (err, response, a) => {
-        let d1 = modules.parser(a)
-        let split = String(url).split("/")
-        let sources = {}
+this.modules.request(false).get(this.url + "/download", (err, response, a) => {
+    let d1 = this.modules.parser(a)
+    let split = this.url.split("/")
+    let sources = {}
 
-        d1.querySelectorAll(".mt-4 > a").forEach(item => {
-            let quality = item.textContent.replace("Qualidade", "").replace(" ", "")
-            let href = item.getAttribute("href")
-            sources[quality] = () => {
-                modules.request(false).get(href, (err, response, b) => {
-                    let d2 = modules.parser(b)
-                    let key = JSON.parse(d2.querySelector("#__NEXT_DATA__").textContent).buildId
-                    modules.request(false).get(`https://download.betteranime.net/_next/data/${key}/${href.split("/")[3]}.json`, (err, response, c) => {
-                        modules.save(JSON.parse(c).pageProps.anime.path, `./(${quality}) ${split[5]}-${split[6]}.mp4`)
-                    })
+    d1.querySelectorAll(".mt-4 > a").forEach(item => {
+        let quality = item.textContent.replace("Qualidade", "").replace(" ", "")
+        let href = item.getAttribute("href")
+        sources[quality] = () => {
+            this.modules.request(false).get(href, (err, response, b) => {
+                let d2 = modules.parser(b)
+                let key = JSON.parse(d2.querySelector("#__NEXT_DATA__").textContent).buildId
+                this.modules.request(false).get(`https://download.betteranime.net/_next/data/${key}/${href.split("/")[3]}.json`, (err, response, c) => {
+                    this.modules.save(JSON.parse(c).pageProps.anime.path, `./(${quality}) ${split[5]}-${split[6]}.mp4`)
                 })
-            }
-        })
-
-        callback(sources)
+            })
+        }
     })
-}
+
+    this.callback(sources)
+})
