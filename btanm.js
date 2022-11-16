a58a757940e62bbbf78045494a05082cc52136f4
PSalleSDev <74500251+PSalleSDev@users.noreply.github.com>
2022-11-16 00:09:00 -0300

    Update btanm.js

--- btanm.js
+++ btanm.js
@@ -11,7 +11,7 @@ this.modules.request(false).get(this.url + "/download", (err, response, a) => {
                 let d2 = this.modules.parser(b)
                 let key = JSON.parse(d2.querySelector("#__NEXT_DATA__").textContent).buildId
                 this.modules.request(false).get(`https://download.betteranime.net/_next/data/${key}/${href.split("/")[3]}.json`, (err, response, c) => {
-                    this.modules.save(JSON.parse(c).pageProps.anime.path, `./(${quality}) ${split[5]}-${split[6]}.mp4`)
+                    this.modules.save(JSON.parse(c).pageProps.anime.path, `./(${quality}) ${split[5]}-${split[6]}`)
                 })
             })
         }
