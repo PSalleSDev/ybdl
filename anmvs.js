bc13708d61cd389bb388fee455722aad5bf7ee4f
PSalleSDev <74500251+PSalleSDev@users.noreply.github.com>
2022-12-23 04:56:50 -0300

    Update anmvs.js

--- anmvs.js
+++ anmvs.js
@@ -1,6 +1,9 @@
-module.exports = (url, modules, callback) => {
-    modules.request(0).get(url, (err, response, data) => {
-        let document = this.modules.parser(data)
-        callback(SON.parse(document.querySelector(".player-frame > div").getAttribute("wire:initial-data")).serverMemo.data.episodiosLink)
-    })
+module.exports = {
+    id: "Anime Vision",
+    run: (url, modules, callback) => {
+        modules.request(0).get(url, (err, response, data) => {
+            let document = this.modules.parser(data)
+            callback(SON.parse(document.querySelector(".player-frame > div").getAttribute("wire:initial-data")).serverMemo.data.episodiosLink)
+        })
+    }
 }
