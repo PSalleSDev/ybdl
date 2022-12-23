177c63b58ba181716f661bdfe2be299d023cc22e
PSalleSDev <74500251+PSalleSDev@users.noreply.github.com>
2022-12-23 05:07:00 -0300

    Update anmvs.js

--- anmvs.js
+++ anmvs.js
@@ -2,7 +2,7 @@ module.exports = {
     id: "Anime Vision",
     run: (url, modules, callback) => {
         modules.request(0).get(url, (err, response, data) => {
-            let document = this.modules.parser(data)
+            let document = modules.parser(data)
             callback(SON.parse(document.querySelector(".player-frame > div").getAttribute("wire:initial-data")).serverMemo.data.episodiosLink)
         })
     }
