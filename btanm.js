1df7ee1cfd87fd002a564317a3868bdb891ce846
PSalleSDev <74500251+PSalleSDev@users.noreply.github.com>
2022-12-24 02:23:30 -0300

    Update btanm.js

--- btanm.js
+++ btanm.js
@@ -1,5 +1,5 @@
 module.exports = {
-    id: "Better Animes",
+    id: "betteranime.net",
     run: (url, modules, callback) => {
         let sources = {}
 
@@ -9,19 +9,15 @@ module.exports = {
         }
 
         modules.request(0).get(url + "/download", (err, response, a) => {
-            let d1 = modules.parser(a)
-        
+            let d1 = modules.parser(a)        
             let srcs = d1.querySelectorAll(".mt-4 > a")
             srcs.forEach(item => {
                 let quality = item.textContent.replace("Qualidade", "").replace(" ", "")
                 let href = item.getAttribute("href")
-
                 modules.request(0).get(href, (err, response, b) => {
                     let d2 = modules.parser(b)
                     let key = JSON.parse(d2.querySelector("#__NEXT_DATA__").textContent).buildId
-                    modules.request(0).get(`https://download.betteranime.net/_next/data/${key}/${href.split("/")[3]}.json`, (err, response, c) => {
-                       Append(quality, JSON.parse(c).pageProps.anime.path, srcs)
-                    })
+                    modules.request(0).get(`https://download.betteranime.net/_next/data/${key}/${href.split("/")[3]}.json`, (err, response, c) => { Append(quality, JSON.parse(c).pageProps.anime.path, srcs) })
                 })
             })
         })
